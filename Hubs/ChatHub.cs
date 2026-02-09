20145d916a50eeab600b1b2dad9f2fe10b6ca570
Garrett Hensley <ghensley@fwcjua.com>
2026-02-09 15:12:08 -0500

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalBlaze.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message) {
+            // This sends the message to EVERYONE connected
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
